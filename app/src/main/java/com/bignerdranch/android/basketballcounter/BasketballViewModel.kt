a6a39fd6fe5c93bc775d0778e7c2cdfbdfbe9028
mpham@wpi.edu <mpham@wpi.edu>
2020-09-15 18:17:25 +0700

    reset

--- app/src/main/java/com/bignerdranch/android/basketballcounter/BasketballViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/basketballcounter/BasketballViewModel.kt
@@ -3,10 +3,10 @@ package com.bignerdranch.android.basketballcounter
 import androidx.lifecycle.ViewModel
 
 class BasketballViewModel : ViewModel() {
-    var scoreA:Int = 0
-    var scoreB:Int = 0
+    var scoreA: Int = 0
+    var scoreB: Int = 0
 
-    fun updatePoints(team: String, score: Int):Int{
+    fun updatePoints(team: String, score: Int): Int {
         if (team == "A") {
             scoreA += score
             return scoreA
@@ -16,8 +16,9 @@ class BasketballViewModel : ViewModel() {
         }
     }
 
-    fun resetScore(){
+    fun resetScore() {
         scoreA = 0
         scoreB = 0
     }
 }
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
